2ae5bc8b1fea464ae72b90fb1d3d4adc1da38dd3
wuchunfu <319355703@qq.com>
2025-08-03 19:30:25 +0800

    init commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,42 @@
 import { App } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import { constantRoutes } from "@/router/common";
-import { PageRoute } from "@/types/route";
 import { createRouterGuard } from "@/router/permissions";
+import { PageRoute } from "@/types/route";
 
 const basePath = import.meta.env.VITE_BASE_PATH;
 
 /**
- * 创建路由
+ * 创建路由实例
  */
 export const router = createRouter({
   history: createWebHistory(basePath),
   routes: constantRoutes,
   strict: true,
   // 刷新时，滚动条位置还原
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    } else {
-      return { left: 0, top: 0 };
-    }
+  scrollBehavior: (to, from, savedPosition) => {
+    return savedPosition || { left: 0, top: 0 };
   }
 })
 
+/**
+ * 安装路由
+ *
+ * @param app Vue应用实例
+ */
 export const setupRouter = async (app: App) => {
   app.use(router);
   await createRouterGuard(router);
   await router.isReady();
 }
 
-// 加载需要验证的路由
+// 自动加载路由模块
 const modules = import.meta.glob<any>('./modules/**/*.ts', { eager: true });
-export const routeModuleList: PageRoute[] = Object.keys(modules).reduce((list: any, key) => {
+/**
+ * 路由模块列表
+ */
+export const routeModuleList: PageRoute[] = Object.keys(modules).reduce<PageRoute[]>((list: PageRoute[], key: string) => {
   const mod: PageRoute | PageRoute[] = modules[key].default ?? {};
-  const modList = Array.isArray(mod) ? [...mod] : [mod];
-  return [...list, ...modList];
+  const modList = Array.isArray(mod) ? mod : [mod];
+  return list.concat(modList);
 }, []);
--- src/router/permissions/index.ts
+++ src/router/permissions/index.ts
@@ -4,81 +4,95 @@ import 'nprogress/nprogress.css';
 import { useAuthStore, useRouteStore } from '@/store';
 import { RouterEnum } from '@/enums/RouterEnum';
 
-NProgress.configure({ showSpinner: false });
-const whiteList: string[] = [RouterEnum.LOGIN, RouterEnum.REGISTER]
+const whiteList: string[] = [RouterEnum.LOGIN, RouterEnum.REGISTER];
 
 export const getPageTitle = (pageTitle: string): string => {
   const title = import.meta.env.VITE_APP_TITLE;
   if (pageTitle) {
     // 拼接每个路由页面的名称显示在浏览器
-    return `${ pageTitle } - ${ title }`
+    return `${ pageTitle } - ${ title }`;
   }
-  return title
+  return title;
 };
+
+NProgress.configure({ showSpinner: false });
 
 export const createRouterGuard = async (router: Router) => {
   router.beforeEach(async (to, from, next) => {
+    // 判断是否是外链，如果是直接打开网页并拦截跳转
+    if (typeof to.meta.href === 'string') {
+      window.open(to.meta.href);
+      // 取消当前导航
+      return next(false);
+    }
+
     NProgress.start();
     //@ts-ignore
     window.$loadingBar?.start();
-    document.title = getPageTitle(to.meta.title as string);
+    document.title = getPageTitle(typeof to.meta.title === 'string' ? to.meta.title : '');
 
     const authStore = useAuthStore();
     const routeStore = useRouteStore();
 
+    // 判断是否已经登录
     if (authStore.token) {
-      if (to.path === RouterEnum.LOGIN) {
+      // 已登录，访问登录页或注册页，则直接进入首页
+      if (to.path === RouterEnum.LOGIN || to.path === RouterEnum.REGISTER) {
         return next({ path: RouterEnum.INDEX });
-      } else if (whiteList.includes(to.path as string)) {
-        // 在免登录白名单，直接进入
+      }
+
+      // 已登录，判断是否在白名单，如果在则直接进入
+      if (whiteList.includes(to.path)) {
         return next();
-      } else {
-        if (!routeStore.isInitRoute) {
-          // 权限路由未加载，先加载权限路由
-          await routeStore.initRoute();
-          return next({ ...to, replace: true });
-        } else {
-          if (to.name === 'not-found') {
-            const path = to.redirectedFrom?.name === 'root' ? '/' : to.fullPath;
-            return next({
-              path,
-              replace: true,
-              query: to.query,
-              hash: to.hash
-            });
-          } else {
-            return next();
-          }
+      }
+
+      // 判断路由是否加载，未加载则加载
+      if (!routeStore.isInitRoute) {
+        await routeStore.initRoute();
+
+        if (to.name === 'not-found') {
+          // 等待权限路由加载好了，回到之前的路由,否则404
+          const path = to.redirectedFrom?.name === 'root' ? RouterEnum.INDEX : to.fullPath;
+          return next({
+            path: path,
+            replace: true,
+            query: to.query,
+            hash: to.hash,
+          });
         }
-      }
-    } else {
-      if (whiteList.includes(to.path as string)) {
-        // 在免登录白名单，直接进入
+
         return next();
-      } else {
-        const redirect = encodeURIComponent(to.fullPath || RouterEnum.INDEX);
-        // 否则全部重定向到登录页
-        return next({
-          path: RouterEnum.LOGIN,
-          query: {
-            redirect: redirect
-          }
-        });
       }
+
+      return next();
     }
+
+    // 未登录，判断是否在白名单，如果在则直接进入
+    if (whiteList.includes(to.path)) {
+      return next();
+    }
+
+    // 未登录，全部重定向到登录页
+    const redirect = encodeURIComponent(to.fullPath || RouterEnum.INDEX);
+    return next({
+      path: RouterEnum.LOGIN,
+      query: {
+        redirect: redirect
+      }
+    });
   });
 
-  router.afterEach((to) => {
-    document.title = getPageTitle(to.meta.title as string);
+  router.afterEach((to, from) => {
+    document.title = getPageTitle(typeof to.meta.title === 'string' ? to.meta.title : '');
     NProgress.done();
     //@ts-ignore
     window.$loadingBar?.finish();
   });
 
   router.onError((error) => {
-    console.log(error);
+    console.error('Router error:', error);
     NProgress.done();
     //@ts-ignore
-    window.$loadingBar?.finish();
+    window.$loadingBar?.error();
   })
 };
